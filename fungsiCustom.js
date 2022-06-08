1b817b01959f61bd333e950743f5d2f342c12cfe
catur wicaksono <catur.wicaksono.developer@gmail.com>
2022-06-08 12:59:41 +0700

    Tantangan: Callback Part 2

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,8 +19,38 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
 
+  fs.readFile(file1, 'utf-8', (err, data) => {
+
+    const dataFile1 = JSON.parse(data);
+    if (!dataFile1.message.split(" ")[1]) return fnCallback("Data tidak sesuai", null);
+
+    fs.readFile(file2, 'utf-8', (err, data) => {
+
+      const dataFile2 = JSON.parse(data);
+      if (!dataFile2[0].message.split(" ")[1]) return fnCallback("Data tidak sesuai", null);
+
+      fs.readFile(file3, 'utf-8', (err, data) => {
+
+        const dataFile3 = JSON.parse(data);
+        if (!dataFile3[0].data.message.split(" ")[1]) return fnCallback("Data tidak sesuai", null);
+
+        const result = [
+          dataFile1.message.split(" ")[1],
+          dataFile2[0].message.split(" ")[1],
+          dataFile3[0].data.message.split(" ")[1],
+        ]
+
+        return fnCallback(null, result);
+
+      }); // file 3
+
+    }); // file 2
+
+  }); // file 1
+
+};
 // ! JANGAN DIMODIFIKASI
 module.exports = {
   modifyFile1,
